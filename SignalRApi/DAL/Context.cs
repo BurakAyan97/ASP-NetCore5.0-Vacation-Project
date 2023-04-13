c9212b83c54cbc5ead81160bd9587ffedf5f2325
BurakAyan97 <burakayan97@outlook.com>
2023-04-14 01:18:01 +0300

    SignalR added

---
+++ SignalRApi/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRApi.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> opt) :base(opt)
+        {
+            
+        }
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
